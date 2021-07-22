af05755f94a4585ca2899f9db94d253494e320f8
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-07-22 10:21:27 +0530

    Create circleArea_method.java

---
+++ circleArea_method.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+    static void circleArea(){
+     double area, pi,r;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the radius of the circle: ");
+      r=sc.nextDouble();
+      pi=3.14;
+      area = pi*r*r;
+      System.out.println("Area of the circle: "+area);
+    }
+  public static void main (String[] args) {
+     circleArea();
+    }
+}
