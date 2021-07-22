61ad60f8e3155dbb22f66c2035f41444415f3619
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-07-22 10:28:02 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main{
+  public static void main (String[] args) {
+     int a,b,c;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter a number: ");
+     a=sc.nextInt();
+     System.out.println("Enter another number: ");
+     b=sc.nextInt();
+     System.out.println("Numbers before swapping");
+     System.out.println("a: "+a);
+     System.out.println("b: "+b);
+     c=a;
+     a=b;
+     b=c;
+     System.out.println("Numbers after swapping");
+     System.out.println("a: "+a);
+     System.out.println("b: "+b);
+    }
+}
